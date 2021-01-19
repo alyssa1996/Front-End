31d7c7157d1e89dcead2d31be923649b16a7c5f1
지수 박 <alyssapark96@gmail.com>
2021-01-19 23:45:41 +0100

    change minor detail and set the standard

--- Date Calculation/date_calculation.js
+++ Date Calculation/date_calculation.js
@@ -2,9 +2,13 @@ function calcuateDate(){
     var startDate=new Date(document.getElementById('startDate').value);
     var endDate= new Date(document.getElementById('endDate').value);
     var d_day=parseInt((endDate.getTime()-startDate.getTime())/(1000*24*60*60));
+    var today=new Date();
 
     if(d_day<0){
         alert('시작 날짜보다 디데이 날짜가 더 뒤에 있습니다! 다시 선택해주세요:)')
+    }
+    else if(startDate<today){
+        alert('시작날짜가 이미 지났습니다! 오늘 이후의 날짜로 다시 선택해주세요:)')
     }
     else{
         document.getElementById("date").innerHTML = d_day+'일 남았습니다!';
@@ -23,6 +27,12 @@ function untilToday(){
     var startingDate=new Date(document.getElementById('startingDate').value);
     var endingDate=new Date(document.getElementById('endingDate').value);
     var countDate= parseInt((endingDate.getTime()-startingDate.getTime())/(1000*24*60*60))
-
-    document.getElementById('countDate').innerHTML=countDate+'일 되었습니다:)';
+    var today=new Date();
+    
+    if(endingDate>today){
+        alert('끝나는 날짜가 오늘을 넘어갔습니다! 다시 선택해주세요:)')   
+    }
+    else{
+        document.getElementById('countDate').innerHTML=countDate+'일 되었습니다:)';
+    }
 }
