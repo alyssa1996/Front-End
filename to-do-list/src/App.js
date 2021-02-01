cc2e1dd1951ba84b9a68c122ca334ff27434cbf0
지수 박 <alyssapark96@gmail.com>
2021-02-01 23:25:58 +0100

    delete commented part

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -6,13 +6,7 @@ function App() {
 
   const [ContentList,setContent]=useState([]);
   let items=[];
-
-  // function setItem(){
-  //   for (const [index, value] of ContentList.entries()) {
-  //     items.push(<p key={index}>Number#{index+1}: {value}</p>)
-  //   }
-  // }
-
+  
   return (
     <div>
       <form action="/create_process" method="post" onSubmit={function(e){
@@ -20,7 +14,6 @@ function App() {
         const addNewContent=Array.from(ContentList)
         addNewContent.push(e.target.content.value);
         setContent(addNewContent)
-        //setItem();
       }}>
         <label for="to-do">Write down what you have to do  </label>
         <input type="text" id="to-do" name="content"></input>
